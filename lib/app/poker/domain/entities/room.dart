a7f205d56fc37a9892c48284c460774cbf9bc404
Cao Việt Đức <112269340+alexcao194@users.noreply.github.com>
2023-04-19 00:30:08 +0700

    update room entity

--- lib/app/poker/domain/entities/room.dart
+++ lib/app/poker/domain/entities/room.dart
@@ -1,3 +1,4 @@
+import 'card.dart';
 import 'player.dart';
 import 'seat.dart';
 
@@ -10,8 +11,21 @@ class Room {
   final int smallBlind;
   final int bigBlind;
   final int type;
-  // final List<Player> players;
-  // final List<Seat> seats;
+  final List<Player> players;
+  final List<Seat> seats;
+  final List<Card> board;
+  final Object deck;
+  final Object button;
+  final Object turn;
+  final int pot;
+  final int mainPot;
+  final Object callAmount;
+  final int minBet;
+  final int minRaise;
+  final bool handOver;
+  final List<String> winMessage;
+  final List<Object> sidePots;
+  final List<Object> history;
 
 
   Room({
@@ -22,6 +36,21 @@ class Room {
     required this.currentNumberPlayers,
     required this.smallBlind,
     required this.bigBlind,
-    required this.type
+    required this.type,
+    this.board = const [],
+    this.button = const Object(),
+    this.callAmount = const Object(),
+    this.deck = const Object(),
+    this.handOver = true,
+    this.history = const [],
+    this.mainPot = 0,
+    this.pot = 0,
+    this.players = const [],
+    this.seats = const [],
+    this.turn = const Object(),
+    this.sidePots = const [],
+    this.winMessage = const [],
+    this.minBet = 0,
+    this.minRaise = 0
   });
 }
